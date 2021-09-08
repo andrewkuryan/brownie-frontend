1f98941744745733bf38d0aed21aa62b57db29ae
andrewkuryan <Andrew.Kuryan@tut.by>
2021-09-08 20:29:44 +0300

    Add Stepper component

---
+++ src/components/stepper/Stepper.tsx
@@ -0,0 +1,31 @@
+import { FunctionalComponent, Fragment } from 'preact';
+
+import './stepper.styl';
+
+export interface StepperProps {
+    numOfSteps: number;
+    currentStep: number;
+}
+
+export const Stepper: FunctionalComponent<StepperProps> = ({ numOfSteps, currentStep }) => {
+    return (
+        <div class="stepperRoot">
+            {[...Array(numOfSteps).keys()].map(num => (
+                <Fragment>
+                    <p
+                        class={`stepPoint ${currentStep === num + 1 ? 'current' : ''} ${
+                            currentStep > num + 1 ? 'passed' : ''
+                        }`}
+                    >
+                        {num + 1}
+                    </p>
+                    {num < numOfSteps - 1 && (
+                        <div
+                            class={`stepPlaceholder ${currentStep > num + 1 ? 'passed' : ''}`}
+                        />
+                    )}
+                </Fragment>
+            ))}
+        </div>
+    );
+};
--- src/pages/register/RegisterView.tsx
+++ src/pages/register/RegisterView.tsx
@@ -1,21 +1,23 @@
 import { FunctionComponent } from 'preact';
+import { useState } from 'preact/hooks';
 import ContactsStepView from './steps/contacts';
 import { ReduxProps } from '../../Main';
+import Stepper from '@components/stepper';
 
 import './register.styl';
 
 export const RegisterView: FunctionComponent<ReduxProps> = ({ useStore, dispatch }) => {
+    const [currentStep, setCurrentStep] = useState<
+        'contact' | 'verifyContact' | 'fulfill' | 'done'
+    >('contact');
+
     return (
         <div class="registerRoot">
             <div class="stepCard">
                 <div class="stepContent">
                     <ContactsStepView useStore={useStore} dispatch={dispatch} />
                 </div>
-                <div class="stepsWrapper">
-                    <div class="stepPoint">
-                        <p>1</p>
-                    </div>
-                </div>
+                <Stepper numOfSteps={4} currentStep={1}/>
             </div>
         </div>
     );
